859f986030078f50bf6978d7144d906cbc96aa50
Ariana <152944814+aam214@users.noreply.github.com>
2024-08-21 22:58:24 -0500

    Edited margin and size of image in game div. Moved monkey image out of images folder. Removed monkey class from CSS. In JS referenced currentMonkey at the top of the file. Set score equal to 0 and gameOver as false. Added setInterval for showBug and showMonkey. Edited name of function to getRandomImg. Added function of showMonkey and selectHole. Added monkey image. If statement added to the showMonkey and showBug functions so they don't show at same hole at the same time. Edited innerText for score and game over with an if/ else satement.

--- game.js
+++ game.js
@@ -3,27 +3,71 @@ window.addEventListener('load', function() {
   setUpGame();
 });
 let currentBug;
+let currentMonkey;
+let score = 0;
+let gameOver = false;
+
 function setUpGame() {
   for (let i= 0; i < 9; i++) {
     let hole = document.createElement("div");
     hole.id = i.toString();
+    hole.addEventListener("click", selectHole);
     document.getElementById("game").appendChild(hole);
   }
-  setInterval(showBug, 2000);
+  setInterval(showBug, 1500);
+  setInterval(showMonkey, 3000)
 }
-function getRandomBug() {
+
+function getRandomImg() {
   let random = Math.floor(Math.random() * 9);
   return random.toString();
 }
 
-function showBug(){
+function showBug() {
+if (gameOver) {
+  return;
+}
 if (currentBug) {
 currentBug.innerHTML = '';
 }
-
 let bug = document.createElement("img");
 bug.src = "./bug.png";
-let random = getRandomBug();
+let random = getRandomImg();
+if(currentMonkey && currentMonkey.id == random) {
+  return;
+}
+
 currentBug = document.getElementById(random);
 currentBug.appendChild(bug);
+}
+
+function showMonkey() {
+  if (gameOver) {
+    return;
+  }
+if (currentMonkey) {
+  currentMonkey.innerHTML = "";
+}
+let monkey = document.createElement("img");
+monkey.src ="./monkey.png";
+
+let random = getRandomImg();
+if(currentBug && currentBug.id == random) {
+  return;
+}
+currentMonkey = document.getElementById(random);
+currentMonkey.appendChild(monkey);
+}
+
+function selectHole() {
+  if (gameOver) {
+    return;
+  }
+  if (this == currentBug) {
+    score +=10;
+    document.getElementById("score").innerHTML=
+    score.toString();
+  }
+  else (this== currentMonkey).innerHTML= "Game Over!" + score.toString();
+  gameOver = true;
 }
