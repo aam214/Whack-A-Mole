069e30bf2798e553ebbd1317320bf8d926d1c453
Ariana <152944814+aam214@users.noreply.github.com>
2024-08-25 18:24:12 -0500

    Moved document.getElementID for game at top of JS file. Changed variable of random to shown for showMonkey( ) and showBug( ).

--- game.js
+++ game.js
@@ -1,43 +1,46 @@
-
 window.addEventListener('load', function() {
   setUpGame();
 });
+
 let currentBug;
 let currentMonkey;
 let score = 0;
 let gameOver = false;
+let game = document.getElementById("game");
+
+
 
 function setUpGame() {
   for (let i= 0; i < 9; i++) {
-    let hole = document.createElement("div");
-    hole.id = i.toString();
-    hole.addEventListener("click", selectHole);
-    document.getElementById("game").appendChild(hole);
+  let hole = document.createElement("div");
+  hole.id = i.toString();
+  hole.addEventListener("click", selectHole);
+  game.appendChild(hole);
   }
   setInterval(showBug, 1000);
-  setInterval(showMonkey, 2000)
+  setInterval(showMonkey, 2000);
 }
 
-function getRandomImg() {
+function showRandomImg() {
   let random = Math.floor(Math.random() * 9);
   return random.toString();
 }
 
 function showBug() {
 if (gameOver) {
-  return;
+return;
 }
 if (currentBug) {
 currentBug.innerHTML = '';
 }
 let bug = document.createElement("img");
 bug.src = "./bug.png";
-let random = getRandomImg();
-if (currentMonkey && currentMonkey.id == random) {
-  return;
+let shown = showRandomImg();
+if (currentMonkey && currentMonkey.id == shown) {
+return;
 }
 
-currentBug = document.getElementById(random);
+currentBug = document.getElementById(shown);
 currentBug.appendChild(bug);
 }
 
@@ -50,12 +53,11 @@ if (currentMonkey) {
 }
 let monkey = document.createElement("img");
 monkey.src ="./monkey.png";
-
-let random = getRandomImg();
-if (currentBug && currentBug.id == random) {
+let shown = showRandomImg();
+if (currentBug && currentBug.id == shown) {
   return;
 }
-currentMonkey = document.getElementById(random);
+currentMonkey = document.getElementById(shown);
 currentMonkey.appendChild(monkey);
 }
 
